a89e41f7c51353a0b4195cf3146ffaf65820c9f1
Tiago Coutinho <coutinho@pccoutinho>
2014-04-20 12:33:57 +0200

    Add helper Qt module
    Helper Qt module that gatters symbols from QtCore and QtGui (only).

---
+++ qarbon/external/qt/Qt.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# This file is part of qarbon (http://qarbon.rtfd.org/)
+#
+# Copyright (c) 2013 European Synchrotron Radiation Facility, Grenoble, France
+#
+# Distributed under the terms of the GNU Lesser General Public License,
+# either version 3 of the License, or (at your option) any later version.
+# See LICENSE.txt for more info.
+# ----------------------------------------------------------------------------
+
+"""Helper Qt module that gatters symbols from QtCore and QtGui (only)."""
+
+from .QtCore import *
+from .QtGui import *
